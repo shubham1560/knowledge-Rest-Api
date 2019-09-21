4d32142cea459cc959b8e0b42537e092f259da2e
Shubham <shubhamsinha2050@gmail.com>
2019-09-21 23:49:33 +0530

    Finalised the table for articles

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('article_number', 'active', 'created_on', 'parent_article', 'workflow_state', 'article_category')
+
+
+admin.site.register(Article, ArticleAdmin)
