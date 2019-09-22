d56126aaa78ea52224905ad7331e454357076f14
Shubham <shubhamsinha2050@gmail.com>
2019-09-22 13:53:55 +0530

    Some admin site manipulations

--- article/admin.py
+++ article/admin.py
@@ -6,6 +6,9 @@ from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('article_number', 'active', 'created_on', 'parent_article', 'workflow_state', 'article_category')
+    # fields = ['article_number', 'workflow_state']  # TO control fields that we can see in form
+    list_filter = ['workflow_state', 'author', 'article_category']
+    search_fields = ['topic']
 
 
 admin.site.register(Article, ArticleAdmin)
