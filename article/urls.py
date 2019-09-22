bda4fc4781331a571cc5d0141d264d93928a8d99
Shubham <shubhamsinha2050@gmail.com>
2019-09-22 15:37:59 +0530

    made an api of articles

--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 from . import views
 from .views import Another
+from rest_framework import routers
+from .views import AricleViewSet
+
+
+router = routers.DefaultRouter()
+router.register('articles', AricleViewSet)
+
 
 urlpatterns = [
-    path('', views.first, name="home"),
-    path('classCall', Another.as_view())
+    # path('', views.first, name="home"),
+    path('', include(router.urls)),
+    path('classCall', Another.as_view()),
 ]
--- article/views.py
+++ article/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Article
+from rest_framework import viewsets
+from .serializers import ArticleSerializer
 # Create your views here.
 
+
+class AricleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
 # class inheriting View already has many things configured for us but it makes the program complex
 class Another(View):
@@ -14,7 +20,7 @@ class Another(View):
         for article in articles:
             print(article.author)
             print(article.article_number)
-        return HttpResponse("Working with "+ article_by_id.article_number+" article")
+        return HttpResponse("Working with " + article_by_id.article_number+" article")
 
 
 def first(request):
