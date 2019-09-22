03de21b750d162e95f78145de925e4232442d692
Shubham <shubhamsinha2050@gmail.com>
2019-09-22 11:09:38 +0530

    used objects

--- article/views.py
+++ article/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Article
 # Create your views here.
 
 
+# class inheriting View already has many things configured for us but it makes the program complex
 class Another(View):
 
     def get(self, request):
-        return HttpResponse("Working with the class too")
+        articles = Article.objects.all()
+        for article in articles:
+            print(article.author)
+            print(article.article_number)
+        return HttpResponse("Working with "+str(len(articles))+" articles")
 
 
 def first(request):
