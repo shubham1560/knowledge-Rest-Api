beb2897851137ea405fdd247fd74abc3b07c6cd6
Shubham <shubhamsinha2050@gmail.com>
2019-09-22 23:23:53 +0530

    permission classes require user to login but authentication classes require the token to meke the session, postman can't operate in permission class

--- article/views.py
+++ article/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from .models import Article
 from rest_framework import viewsets
 from .serializers import ArticleSerializer, AuthorSerializer, ArticleMiniSerializer
-# from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 from django.contrib.auth.models import User
@@ -20,19 +20,20 @@ from rest_framework.response import Response
 class AricleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleMiniSerializer
     queryset = Article.objects.all()
-    # authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)  # To restrict permission to this single model or viewSet
+    authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)  # To restrict permission to this single model or viewSet
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = ArticleSerializer(instance)
         return Response(serializer.data)
 
+
 class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     queryset = User.objects.all()
-    # authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)  # To restrict permission to this single model or viewSet
+    authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)  # To restrict permission to this single model or viewSet
 
 # class inheriting View already has many things configured for us but it makes the program complex
 
